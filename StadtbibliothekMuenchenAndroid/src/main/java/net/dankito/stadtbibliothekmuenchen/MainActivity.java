097552f6b2b70ae6b1a1f0cfdcb5b526ec48a0e7
dankito <dankito@user.noreply.github.com>
2016-11-25 16:09:15 +0100

    Replaced SectionsPagerAdapter by MainActivityTabsAdapter

--- StadtbibliothekMuenchenAndroid/src/main/java/net/dankito/stadtbibliothekmuenchen/MainActivity.java
+++ StadtbibliothekMuenchenAndroid/src/main/java/net/dankito/stadtbibliothekmuenchen/MainActivity.java
@@ -7,7 +7,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.os.Bundle;
@@ -19,6 +18,8 @@ import android.view.ViewGroup;
 
 import android.widget.TextView;
 
+import net.dankito.stadtbibliothekmuenchen.adapter.MainActivityTabsAdapter;
+
 public class MainActivity extends AppCompatActivity {
 
   /**
@@ -29,12 +30,12 @@ public class MainActivity extends AppCompatActivity {
    * may be best to switch to a
    * {@link android.support.v4.app.FragmentStatePagerAdapter}.
    */
-  private SectionsPagerAdapter mSectionsPagerAdapter;
+  protected MainActivityTabsAdapter tabsAdapter;
 
   /**
    * The {@link ViewPager} that will host the section contents.
    */
-  private ViewPager mViewPager;
+  protected ViewPager viewPager;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -43,16 +44,15 @@ public class MainActivity extends AppCompatActivity {
 
     Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
     setSupportActionBar(toolbar);
-    // Create the adapter that will return a fragment for each of the three
-    // primary sections of the activity.
-    mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+
+    tabsAdapter = new MainActivityTabsAdapter(this, getSupportFragmentManager());
 
     // Set up the ViewPager with the sections adapter.
-    mViewPager = (ViewPager) findViewById(R.id.container);
-    mViewPager.setAdapter(mSectionsPagerAdapter);
+    viewPager = (ViewPager) findViewById(R.id.container);
+    viewPager.setAdapter(tabsAdapter);
 
     TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
-    tabLayout.setupWithViewPager(mViewPager);
+    tabLayout.setupWithViewPager(viewPager);
 
     FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
     fab.setOnClickListener(new View.OnClickListener() {
@@ -123,40 +123,4 @@ public class MainActivity extends AppCompatActivity {
     }
   }
 
-  /**
-   * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
-   * one of the sections/tabs/pages.
-   */
-  public class SectionsPagerAdapter extends FragmentPagerAdapter {
-
-    public SectionsPagerAdapter(FragmentManager fm) {
-      super(fm);
-    }
-
-    @Override
-    public Fragment getItem(int position) {
-      // getItem is called to instantiate the fragment for the given page.
-      // Return a PlaceholderFragment (defined as a static inner class below).
-      return PlaceholderFragment.newInstance(position + 1);
-    }
-
-    @Override
-    public int getCount() {
-      // Show 3 total pages.
-      return 3;
-    }
-
-    @Override
-    public CharSequence getPageTitle(int position) {
-      switch (position) {
-        case 0:
-          return "SECTION 1";
-        case 1:
-          return "SECTION 2";
-        case 2:
-          return "SECTION 3";
-      }
-      return null;
-    }
-  }
 }
