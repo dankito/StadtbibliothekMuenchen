94cfbe075ceff103c52169c1b378e97d6c9e8089
dankito <dankito@user.noreply.github.com>
2016-11-25 17:05:56 +0100

    Added AndroidDiComponent and -Container

---
+++ StadtbibliothekMuenchenAndroid/src/main/java/net/dankito/stadtbibliothekmuenchen/di/AndroidDiComponent.java
@@ -0,0 +1,22 @@
+package net.dankito.stadtbibliothekmuenchen.di;
+
+import net.dankito.stadtbibliothekmuenchen.MainActivity;
+import net.dankito.stadtbibliothekmuenchen.fragments.TabBorrowsFragment;
+
+import javax.inject.Singleton;
+
+import dagger.Component;
+
+/**
+ * Created by ganymed on 03/11/16.
+ */
+@Singleton
+@Component(modules = { AndroidDiContainer.class } )
+public interface AndroidDiComponent {
+
+  // to update the fields in your activities
+  void inject(MainActivity activity);
+
+  void inject(TabBorrowsFragment tabBorrowsFragment);
+
+}
---
+++ StadtbibliothekMuenchenAndroid/src/main/java/net/dankito/stadtbibliothekmuenchen/di/AndroidDiContainer.java
@@ -0,0 +1,46 @@
+package net.dankito.stadtbibliothekmuenchen.di;
+
+import android.app.Activity;
+
+import net.dankito.stadtbibliothekmuenchen.StadtbibliothekMuenchenClient;
+import net.dankito.stadtbibliothekmuenchen.util.web.IWebClient;
+import net.dankito.stadtbibliothekmuenchen.util.web.OkHttpWebClient;
+
+import javax.inject.Singleton;
+
+import dagger.Module;
+import dagger.Provides;
+
+/**
+ * Created by ganymed on 03/11/16.
+ */
+@Module
+public class AndroidDiContainer {
+
+  protected final Activity activity;
+
+
+  public AndroidDiContainer (Activity activity) {
+    this.activity = activity;
+  }
+
+
+  @Provides //scope is not necessary for parameters stored within the module
+  public Activity getActivity() {
+    return activity;
+  }
+
+
+  @Provides
+  @Singleton
+  public IWebClient provideWebClient() {
+    return new OkHttpWebClient();
+  }
+
+  @Provides
+  @Singleton
+  public StadtbibliothekMuenchenClient provideStadtbibliothekMuenchenClient(IWebClient webClient) {
+    return new StadtbibliothekMuenchenClient(webClient);
+  }
+
+}
