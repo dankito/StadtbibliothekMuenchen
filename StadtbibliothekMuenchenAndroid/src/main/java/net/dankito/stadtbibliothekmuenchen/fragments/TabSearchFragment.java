c11c3a7c5eac9c849b4c9058e6bf31c96e318433
dankito <dankito@user.noreply.github.com>
2016-11-25 17:32:10 +0100

    Added TabSearchFragment

---
+++ StadtbibliothekMuenchenAndroid/src/main/java/net/dankito/stadtbibliothekmuenchen/fragments/TabSearchFragment.java
@@ -0,0 +1,42 @@
+package net.dankito.stadtbibliothekmuenchen.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+
+import net.dankito.stadtbibliothekmuenchen.MainActivity;
+import net.dankito.stadtbibliothekmuenchen.R;
+import net.dankito.stadtbibliothekmuenchen.adapter.SearchResultsAdapter;
+
+/**
+ * Created by ganymed on 25/11/16.
+ */
+
+public class TabSearchFragment extends Fragment {
+
+  protected SearchResultsAdapter searchResultsAdapter;
+
+
+  @Nullable
+  @Override
+  public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    injectComponents();
+
+    View view = inflater.inflate(R.layout.fragment_tab_search, container, false);
+
+    searchResultsAdapter = new SearchResultsAdapter(getActivity());
+
+    ListView lstvwSearchResults = (ListView)view.findViewById(R.id.lstvwSearchResults);
+    lstvwSearchResults.setAdapter(searchResultsAdapter);
+
+    return view;
+  }
+
+  protected void injectComponents() {
+    ((MainActivity) getActivity()).getComponent().inject(this);
+  }
+}
