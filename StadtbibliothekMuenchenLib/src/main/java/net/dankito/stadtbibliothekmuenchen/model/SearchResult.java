12c24acda1be0b7ddfe6679ffe130dd8cf91c55e
dankito <dankito@user.noreply.github.com>
2016-11-25 20:52:01 +0100

    Added SearchResult fields

--- StadtbibliothekMuenchenLib/src/main/java/net/dankito/stadtbibliothekmuenchen/model/SearchResult.java
+++ StadtbibliothekMuenchenLib/src/main/java/net/dankito/stadtbibliothekmuenchen/model/SearchResult.java
@@ -1,8 +1,83 @@
 package net.dankito.stadtbibliothekmuenchen.model;
 
+import org.jsoup.nodes.Document;
+
 /**
  * Created by ganymed on 25/11/16.
  */
 
 public class SearchResult {
+
+  protected String mediaInfo;
+
+  protected String mediaDetailsToken;
+
+  protected Document searchResultsDocument;
+
+  protected String year;
+
+  protected String mediaTypeIconUrl;
+
+  protected String availabilityIconUrl;
+
+
+  public SearchResult() {
+
+  }
+
+
+  public String getMediaInfo() {
+    return mediaInfo;
+  }
+
+  public void setMediaInfo(String mediaInfo) {
+    this.mediaInfo = mediaInfo;
+  }
+
+  public String getMediaDetailsToken() {
+    return mediaDetailsToken;
+  }
+
+  public void setMediaDetailsToken(String mediaDetailsToken) {
+    this.mediaDetailsToken = mediaDetailsToken;
+  }
+
+  public Document getSearchResultsDocument() {
+    return searchResultsDocument;
+  }
+
+  public void setSearchResultsDocument(Document searchResultsDocument) {
+    this.searchResultsDocument = searchResultsDocument;
+  }
+
+  public String getYear() {
+    return year;
+  }
+
+  public void setYear(String year) {
+    this.year = year;
+  }
+
+  public String getMediaTypeIconUrl() {
+    return mediaTypeIconUrl;
+  }
+
+  public void setMediaTypeIconUrl(String mediaTypeIconUrl) {
+    this.mediaTypeIconUrl = mediaTypeIconUrl;
+  }
+
+  public String getAvailabilityIconUrl() {
+    return availabilityIconUrl;
+  }
+
+  public void setAvailabilityIconUrl(String availabilityIconUrl) {
+    this.availabilityIconUrl = availabilityIconUrl;
+  }
+
+
+  @Override
+  public String toString() {
+    return getMediaInfo();
+  }
+
 }
