61ae3c79496abc6759a99da17ce651f5b0a65c49
dankito <dankito@user.noreply.github.com>
2016-11-30 21:08:45 +0100

    Implemented setting time to check for expirations

--- StadtbibliothekMuenchenAndroid/src/main/java/net/dankito/stadtbibliothekmuenchen/activities/SettingsActivity.java
+++ StadtbibliothekMuenchenAndroid/src/main/java/net/dankito/stadtbibliothekmuenchen/activities/SettingsActivity.java
@@ -1,5 +1,6 @@
 package net.dankito.stadtbibliothekmuenchen.activities;
 
+import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
@@ -11,15 +12,19 @@ import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.TimePicker;
 
 import net.dankito.stadtbibliothekmuenchen.R;
 import net.dankito.stadtbibliothekmuenchen.StadtbibliothekMuenchenApplication;
+import net.dankito.stadtbibliothekmuenchen.fragments.TimePickerFragment;
 import net.dankito.stadtbibliothekmuenchen.model.UserSettings;
 import net.dankito.stadtbibliothekmuenchen.services.StadtbibliothekMuenchenClient;
 import net.dankito.stadtbibliothekmuenchen.services.UserSettingsManager;
 import net.dankito.stadtbibliothekmuenchen.util.AlertHelper;
 import net.dankito.stadtbibliothekmuenchen.util.web.callbacks.LoginCallback;
 import net.dankito.stadtbibliothekmuenchen.util.web.responses.LoginResult;
+
+import java.util.Calendar;
 
 import javax.inject.Inject;
 
@@ -38,6 +43,8 @@ public class SettingsActivity extends AppCompatActivity {
   @Inject
   protected StadtbibliothekMuenchenClient stadtbibliothekMuenchenClient;
 
+  protected Calendar newTimeToCheckForExpirations = null;
+
 
   protected EditText edtxtAddress;
 
@@ -45,12 +52,15 @@ public class SettingsActivity extends AppCompatActivity {
 
   protected CheckBox chkbxPeriodicallyCheckForExpiredBorrows;
 
+  protected TextView txtvwTimeToCheckForExpirations;
+
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
 
     ((StadtbibliothekMuenchenApplication)getApplicationContext()).getComponent().inject(this);
+    this.newTimeToCheckForExpirations = userSettings.getPeriodicalBorrowsExpirationCheckTime();
 
     setupViews();
   }
@@ -72,6 +82,10 @@ public class SettingsActivity extends AppCompatActivity {
     Button btnTestUserSettings = (Button)findViewById(R.id.btnTestUserSettings);
     btnTestUserSettings.setOnClickListener(btnTestUserSettingsClickListener);
 
+    txtvwTimeToCheckForExpirations = (TextView)findViewById(R.id.txtvwTimeToCheckForExpirations);
+    txtvwTimeToCheckForExpirations.setOnClickListener(txtvwTimeToCheckForExpirationsClickListener);
+    showTimeToCheckForExpirations(userSettings.getPeriodicalBorrowsExpirationCheckTime());
+
     chkbxPeriodicallyCheckForExpiredBorrows = (CheckBox)findViewById(R.id.chkbxPeriodicallyCheckForExpiredBorrows);
     chkbxPeriodicallyCheckForExpiredBorrows.setOnCheckedChangeListener(chkbxPeriodicallyCheckForExpiredBorrowsCheckedChangeListener);
     chkbxPeriodicallyCheckForExpiredBorrows.setChecked(userSettings.isPeriodicalBorrowsExpirationCheckTimeSet());
@@ -83,6 +97,10 @@ public class SettingsActivity extends AppCompatActivity {
     btnCancel.setOnClickListener(btnCancelClickListener);
 
     setupToolbar();
+  }
+
+  protected void showTimeToCheckForExpirations(Calendar checkTime) {
+    txtvwTimeToCheckForExpirations.setText(getString(R.string.fragment_setting_time_to_check_for_expirations, checkTime.get(Calendar.HOUR_OF_DAY), checkTime.get(Calendar.MINUTE)));
   }
 
   protected void setupToolbar() {
@@ -143,7 +161,7 @@ public class SettingsActivity extends AppCompatActivity {
       userSettings.setPeriodicalBorrowsExpirationCheckTime(null);
     }
     else {
-
+      userSettings.setPeriodicalBorrowsExpirationCheckTime(newTimeToCheckForExpirations);
     }
 
     try {
@@ -164,7 +182,32 @@ public class SettingsActivity extends AppCompatActivity {
   protected CompoundButton.OnCheckedChangeListener chkbxPeriodicallyCheckForExpiredBorrowsCheckedChangeListener = new CompoundButton.OnCheckedChangeListener() {
     @Override
     public void onCheckedChanged(CompoundButton compoundButton, boolean checked) {
+      txtvwTimeToCheckForExpirations.setEnabled(checked);
+    }
+  };
 
+  View.OnClickListener txtvwTimeToCheckForExpirationsClickListener = new View.OnClickListener() {
+    @Override
+    public void onClick(View view) {
+      TimePickerFragment timePickerFragment = new TimePickerFragment();
+      timePickerFragment.setListener(timePickerListener);
+
+      Calendar checkTime = userSettings.getPeriodicalBorrowsExpirationCheckTime();
+      timePickerFragment.setHourToShow(checkTime.get(Calendar.HOUR_OF_DAY));
+      timePickerFragment.setMinuteToShow(checkTime.get(Calendar.MINUTE));
+
+      timePickerFragment.show(getSupportFragmentManager(), "timePicker");
+    }
+  };
+
+  protected TimePickerDialog.OnTimeSetListener timePickerListener = new TimePickerDialog.OnTimeSetListener() {
+    public void onTimeSet(TimePicker view, int selectedHour,
+                          int selectedMinute) {
+      newTimeToCheckForExpirations = Calendar.getInstance();
+      newTimeToCheckForExpirations.set(Calendar.HOUR_OF_DAY, selectedHour);
+      newTimeToCheckForExpirations.set(Calendar.MINUTE, selectedMinute);
+
+      showTimeToCheckForExpirations(newTimeToCheckForExpirations);
     }
   };
 
